d7563a5158ec8d9d28d4a71c518653f51c0c9228
Tewos-21 <tewodrosshimels54@gmail.com>
2024-12-22 10:33:06 -0800

    deploye

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/", // Set to default
   plugins: [react()],
+  base: "/Tewodros-portfolio", // Set to default
 });
 
