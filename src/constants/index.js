7b7bbfbb68071c88e2345f92f329e49afac36a32
Tewos-27 <tewodrosshimels54@gmail.com>
2026-05-09 12:35:37 +0300

    fix portfolio

--- src/constants/index.js
+++ src/constants/index.js
@@ -135,7 +135,7 @@ const experiences = [
     iconBg: "#383E56",
     points: [
       "Developed and maintained a Dashen superapp platform using microservices and monorepo architecture.",
-      "Integrated banking and payment services (Dashen Bank, M-Pesa, Telebirr)",
+      "Integrated banking and payment services",
       "Implemented Role based authentication and user management",
     ],
   },
